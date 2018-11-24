862d623dd37d339b169370e57aa21b9bed3161c5
Juan Carlos Argumedo Morelos <juarmos@hotmail.com>
2018-11-23 21:11:40 -0500

    CRUD-Backend-API

---
+++ Sales1.0/Sales1.0.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,9 @@
+﻿namespace Sales1._0.Backend.Models
+{
+    using Sales1._0.Domain.Models;
+
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<Sales1._0.Common.Models.Product> Products { get; set; }
+    }
+}
---
+++ Sales1.0/Sales1.0.Domain/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Sales1._0.Domain.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Sales1._0.Common.Models.Product> Products { get; set; }
+    }
+}
